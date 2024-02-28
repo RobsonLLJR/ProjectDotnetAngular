9068a4838d479c2404076e51bcbb2261b2a1c153
Robson Liesner de Lima Junior <robsonlljr@gmail.com>
2024-02-28 09:34:36 -0300

    Instalado bootstrap e adicionado componente Navbar em index.

--- ProjectAngular.UI/src/app/app.routes.ts
+++ ProjectAngular.UI/src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
+NgModule({
+    declarations: [NavbarComponent]
+})
 
 export const routes: Routes = [];
