6185c56ad13f6da6c389bb3412999a53c5a68758
Robson Liesner de Lima Junior <robsonlljr@gmail.com>
2024-02-28 16:38:18 -0300

    Adicionado cabeçalho na listagem dos produtos e configurados css para exbição
    - Renomeado rota de página
    - Trocado container-fluid por container
    - Adicionado css para melhoria de exibição de tabela

--- ProjectAngular.UI/src/app/app.routes.ts
+++ ProjectAngular.UI/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { ProdutoListComponent } from './features/produto/produto-list/produto-list.component';
 import { HttpClient } from '@angular/common/http';
-
 NgModule({
     declarations: [NavbarComponent],
     imports:[
@@ -13,7 +12,7 @@ NgModule({
 
 export const routes: Routes = [
     {
-        path: 'admin/produtos',
+        path: 'produtos',
         component: ProdutoListComponent
     }
 ];
